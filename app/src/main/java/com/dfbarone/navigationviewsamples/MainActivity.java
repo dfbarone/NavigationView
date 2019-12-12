a92d8829688508e30ea822b0b3445db5963910a0
Dominic Barone <dominic.f.barone@gmail.com>
2019-12-12 12:44:48 -0500

    Add sample app.

---
+++ app/src/main/java/com/dfbarone/navigationviewsamples/MainActivity.java
@@ -0,0 +1,37 @@
+package com.dfbarone.navigationviewsamples;
+
+import android.os.Bundle;
+import com.dfbarone.android.navigationview.widget.NavigationView;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        NavigationView leftNavView = findViewById(R.id.left_nav_view);
+        BottomNavigationView bottomNavView = findViewById(R.id.nav_view);
+        // Passing each menu ID as a set of Ids because each
+        // menu should be considered as top level destinations.
+        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
+                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications)
+                .build();
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
+        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
+
+        if (bottomNavView != null) {
+            NavigationUI.setupWithNavController(bottomNavView, navController);
+        }
+
+        if (leftNavView != null) {
+
+        }
+
+    }
+}
